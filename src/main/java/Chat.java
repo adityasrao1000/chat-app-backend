37df0790907ab3947dc740446457abbd2854d6a8
Aditya Rao <adityasrao1000@gmail.com>
2018-08-26 13:55:15 +0530

    restructured to support multiple chatrooms

--- src/main/java/Chat.java
+++ src/main/java/Chat.java
@@ -1,39 +1,17 @@
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
-import org.eclipse.jetty.websocket.api.Session;
-import org.json.JSONObject;
 import static spark.Spark.*;
 
 public class Chat {
 
 	// this map is shared between sessions and threads, so it needs to be
 	// thread-safe (http://stackoverflow.com/a/2688817)
-	static Map<Session, String> userUsernameMap = new ConcurrentHashMap<>();
-	static int nextUserNumber = 1; // Assign to user name for next connecting user
+	
 
 	public static void main(String[] args) {
 		// staticFiles.location("/public"); // index.html is served at localhost:4567
 		// (default port)
 		// staticFiles.expireTime(600);
 		webSocket("/chat", ChatWebSocketHandler.class);
+		webSocket("/chat1", ChatWebSocketHandler.class);
 		init();
 	}
-
-	// Sends a message from one user to all users, along with a list of current
-	// user names
-	public static void broadcastMessage(String sender, String message) {
-		userUsernameMap.keySet().stream().filter(Session::isOpen).forEach(session -> {
-			try {
-				session.getRemote()
-						.sendString(String.valueOf(new JSONObject().put("sender", sender)
-								.put("timestamp", new SimpleDateFormat("HH:mm:ss").format(new Date()))
-								.put("message", message).put("userlist", userUsernameMap.values())));
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-		});
-	}
-
 }
--- src/main/java/ChatWebSocketHandler.java
+++ src/main/java/ChatWebSocketHandler.java
@@ -1,27 +1,50 @@
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 import org.eclipse.jetty.websocket.api.*;
 import org.eclipse.jetty.websocket.api.annotations.*;
+import org.eclipse.jetty.websocket.client.ClientUpgradeRequest;
+import org.json.JSONObject;
 
 @WebSocket
-public class ChatWebSocketHandler {
+final public class ChatWebSocketHandler {
+	private Map<Session, String> userUsernameMap = new ConcurrentHashMap<>();
+	private int nextUserNumber = 1; // Assign to user name for next connecting user
+    private static ClientUpgradeRequest request = new ClientUpgradeRequest();
+    
+	@OnWebSocketConnect
+	public void onConnect(Session user) throws Exception {
+		request.getHeader("username");
+		String username = "User" + nextUserNumber++;
+		userUsernameMap.put(user, username);
+		broadcastMessage("Server", (username + " joined the chat"));
+	}
 
+	@OnWebSocketClose
+	public void onClose(Session user, int statusCode, String reason) {
+		String username = userUsernameMap.get(user);
+		userUsernameMap.remove(user);
+		broadcastMessage("Server", (username + " left the chat"));
+	}
 
-    @OnWebSocketConnect
-    public void onConnect(Session user) throws Exception {
-        String username = "User" + Chat.nextUserNumber++;
-        Chat.userUsernameMap.put(user, username);
-        Chat.broadcastMessage("Server", (username + " joined the chat"));
-    }
-
-    @OnWebSocketClose
-    public void onClose(Session user, int statusCode, String reason) {
-        String username = Chat.userUsernameMap.get(user);
-        Chat.userUsernameMap.remove(user);
-        Chat.broadcastMessage("Server", (username + " left the chat"));
-    }
-
-    @OnWebSocketMessage
+	@OnWebSocketMessage
 	public void onMessage(Session user, String message) {
-        Chat.broadcastMessage(Chat.userUsernameMap.get(user), message);
-    }
- 
+		broadcastMessage(userUsernameMap.get(user), message);
+	}
+
+	// Sends a message from one user to all users, along with a list of current
+	// user names
+	private void broadcastMessage(String sender, String message) {
+		userUsernameMap.keySet().stream().filter(Session::isOpen).forEach(session -> {
+			try {
+				session.getRemote()
+						.sendString(String.valueOf(new JSONObject().put("sender", sender)
+								.put("timestamp", new SimpleDateFormat("HH:mm:ss").format(new Date()))
+								.put("message", message).put("userlist", userUsernameMap.values())));
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		});
+	}
 }
