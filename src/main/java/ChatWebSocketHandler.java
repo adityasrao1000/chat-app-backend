05dfeb2fb8ca5843cf1eab15d08165b8a4c4a158
Aditya Rao <adityasrao1000@gmail.com>
2018-08-28 16:13:51 +0530

    changes made

--- src/main/java/ChatWebSocketHandler.java
+++ src/main/java/ChatWebSocketHandler.java
@@ -1,7 +1,7 @@
-import java.net.InetSocketAddress;
 import java.nio.ByteBuffer;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ExecutionException;
@@ -13,22 +13,32 @@ import org.json.JSONObject;
 @WebSocket(maxTextMessageSize = 1048576, maxBinaryMessageSize = 10485760)
 final public class ChatWebSocketHandler {
 	private Map<Session, String> userUsernameMap = new ConcurrentHashMap<>();
-	private int nextUserNumber = 1; // Assign to user name for next connecting user
 
 	@OnWebSocketConnect
 	public void onConnect(Session user) throws Exception {
-		String username = "User" + nextUserNumber++;
-		final InetSocketAddress remoteAddr = user.getRemoteAddress();
-		System.out.println(remoteAddr);
-		userUsernameMap.put(user, username);
-		broadcastMessage("Server", (username + " joined the chat"));
+		List<String> name = user.getUpgradeRequest().getParameterMap().get("name");
+		if (name.get(0) != null) {
+			if (userUsernameMap.containsValue(name.get(0))) {
+				user.close();
+			} else {
+				userUsernameMap.put(user, name.get(0));
+				broadcastMessage("Server", (name.get(0) + " joined the chat"));
+			}
+		}
 	}
 
 	@OnWebSocketClose
 	public void onClose(Session user, int statusCode, String reason) {
 		String username = userUsernameMap.get(user);
-		userUsernameMap.remove(user);
-		broadcastMessage("Server", (username + " left the chat"));
+		if (username != null) {
+			userUsernameMap.remove(user);
+			broadcastMessage("Server", (username + " left the chat"));
+		}
+	}
+
+	@OnWebSocketError
+	public void onError(Throwable error) {
+		error.printStackTrace();
 	}
 
 	@OnWebSocketMessage
@@ -47,8 +57,7 @@ final public class ChatWebSocketHandler {
 		userUsernameMap.keySet().stream().filter(Session::isOpen).forEach(session -> {
 			try {
 				session.getRemote()
-						.sendString(String.valueOf(new JSONObject().put("sender", sender)
-								.put("type", "text")
+						.sendString(String.valueOf(new JSONObject().put("sender", sender).put("type", "text")
 								.put("timestamp", new SimpleDateFormat("HH:mm:ss").format(new Date()))
 								.put("message", message).put("userlist", userUsernameMap.values())));
 			} catch (Exception e) {
