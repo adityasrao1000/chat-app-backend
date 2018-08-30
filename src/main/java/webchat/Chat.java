3c6a398c601a1fa0d0e15eb7becbe7cd15824f22
Aditya Rao <adityasrao1000@gmail.com>
2018-08-30 16:26:26 +0530

    changes made

--- src/main/java/webchat/Chat.java
+++ src/main/java/webchat/Chat.java
@@ -1,11 +1,12 @@
 package webchat;
 
-import static spark.Spark.*;
+import static spark.Spark.webSocket;
+import static spark.Spark.init;
 
-public class Chat {
+final public class Chat {
 
 	public static void main(String[] args) {
-		// served at localhost:4567
+		// served at local host:4567
 		webSocket("/chat", ChatWebSocketHandler.class);
 		webSocket("/chat1", ChatWebSocketHandler.class);
 		init();
